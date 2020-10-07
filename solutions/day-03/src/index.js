cee1327222d8264871de22b0211816ce1329d076
John Ayling <john.ayling@digintent.com>
2020-10-06 19:42:55 -0500

    Add card.

--- solutions/day-03/src/index.js
+++ solutions/day-03/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import cssLogo from './images/css_logo.png'
 import htmlLogo from './images/html_logo.png'
 import reactLogo from './images/react_logo.png'
+import john from './images/john.jpg'
 
 const header = (
   <div>
@@ -15,9 +16,44 @@ const header = (
   </div>
 );
 
+const avatar = (
+  <div className='avatar-root'>
+    <img className='avatar-image' src={john} alt='John ayling'/>
+    <p className='avatar-name'>John Ayling</p>
+    <p className='avatar-name'>Programmer</p>
+  </div>
+)
+
+const skillHtml = (skill) => (
+  <p key={skill} className='skill'>{skill}</p>
+)
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JS',
+  'jQuery',
+  'React',
+  'React Native',
+  'PHP',
+  'C#',
+  'Unity',
+];
+
+const card = (
+  <div className='card'>
+    {avatar}
+    <div>Skills</div>
+    <div className='skill-section'>
+      {skills.map(skill => skillHtml(skill))}
+    </div>
+  </div>
+)
+
 const app = (
   <div>
     {header}
+    {card}
   </div>
 );
 
