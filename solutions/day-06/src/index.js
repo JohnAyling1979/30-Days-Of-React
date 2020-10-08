4859b863cd9ab0560ae94561c9d98d7ff85001b6
John Ayling <john.ayling@digintent.com>
2020-10-07 21:11:51 -0500

    Day 6

--- solutions/day-06/src/index.js
+++ solutions/day-06/src/index.js
@@ -1,42 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
 // importing data
-
-import { countriesData } from './data/countries'
-import { tenMostHighestPopulations } from './data/ten_most_highest_populations'
-
-const countries = [
-  { name: 'Finland', city: 'Helsinki' },
-  { name: 'Sweden', city: 'Stockholm' },
-  { name: 'Denmark', city: 'Copenhagen' },
-  { name: 'Norway', city: 'Oslo' },
-  { name: 'Iceland', city: 'Reykjavík' },
-]
-
-// Country component
-const Country = ({ country: { name, city } }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <small>{city}</small>
-    </div>
-  )
-}
-
-// countries component
-const Countries = ({ countries }) => {
-  const countryList = countries.map((country) => (
-    <Country key={country.name} country={country} />
-  ))
-  return <div>{countryList}</div>
-}
+import { tenHighestPopulation } from './data/ten_most_highest_populations'
 
 const numbers = [];
+const colors = [];
 
 for (let i = 0; i < 32; i++) {
   numbers.push(i);
+}
+
+for (let i = 0; i <= 32; i++) {
+  let str = '0123456789abcdef'
+  let color = ''
+
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length)
+    color += str[index]
+  }
+
+  colors.push('#' + color);
 }
 
 const PrimeSquare = ({number}) => {
@@ -44,9 +28,9 @@ const PrimeSquare = ({number}) => {
   let color = ''
 
   if (isEven) {
-    color = 'green';
+    color = 'orange';
   } else {
-    color = 'yellow';
+    color = 'purple';
   }
 
   let prime = number > 1;
@@ -68,16 +52,43 @@ const PrimeSquare = ({number}) => {
   );
 }
 
+const ColorSquare = ({color}) => (
+  <div className='square' style={{background: color}}>
+    <p>{color}</p>
+  </div>
+)
+
+const total = tenHighestPopulation[0].population;
+
+const Country = ({country, population}) => {
+  const width = population / total * 100 + '%';
+  return (
+    <div>
+      <p className='country'>{country}</p>
+      <p className='bar-section'>
+        <p style={{height: '24px', width, background: 'orange'}}/>
+      </p>
+      <p className='population'>{population}</p>
+    </div>
+  )
+} 
+
 // The App, or the parent or the container component
 // Functional Component
 const App = () => {
   return (
     <div className='app'>
       <div>
-      <h2>30 Days of React</h2>
-      <h3>Number Generator</h3>
-      {numbers.map(number => <PrimeSquare key={number} number={number} />)}
+        <h2>30 Days of React</h2>
+        <h3>Number Generator</h3>
+        {numbers.map(number => <PrimeSquare key={number} number={number} />)}
 
+        <h2>30 Days of React</h2>
+        <h3>Hex Colors</h3>
+        {colors.map((color, index) => <ColorSquare key={index} color={color} />)}
+
+        <h3>World Population</h3>
+        {tenHighestPopulation.map(data => <Country key={data.country} country={data.country} population={data.population}/>)}
       </div>
     </div>
   )
